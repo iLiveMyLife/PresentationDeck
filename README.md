701bede59a34750d8b04b9423c50384fe145adf3
Meerim Maratova <meerimara@gmail.com>
2021-08-13 13:51:02 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# PresentationDeck
+Presentation Deck is a component to create visual presentations of information. Usually it is used on landing pages to showcase use cases or other similar information
---
+++ presentation /Presentation.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import {Button,Carousel} from 'antd';
+import './Presentation.css';
+import slideData from './data.json';
+import sloganPicture from '../../public/slogans/ComputerBrain.jpg';
+
+class Presentation extends Component {
+  state={
+    data:slideData
+  }
+    render() {
+        return (
+          <Carousel>
+            <div className={"presentaion-center"}>
+                <h2>Congratulations,</h2>
+                <h3>You are a step closer to merging your Life into the Graph-mind.</h3>
+                <br />
+                <h4>Start collaborating with others on common projects by merging into your new Life!</h4>
+                <Button type="primary">
+                    <a href="https://app.ilivemylife.io/signup">Merge Now</a>
+                </Button>
+            </div>
+            {this.state.data.map((item,index)=>(
+              <div className={"carousel-style"}>
+                <h3>{item.title}</h3>
+                <h4>{item.description}</h4>
+                {item.image ? <img className="slogan-pic" src={item.image} /> : null}
+                </div>
+              ))}
+                </Carousel>
+        );
+    }
+}
+
+export default Presentation;
